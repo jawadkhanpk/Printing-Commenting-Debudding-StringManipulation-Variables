fff920579d22b6833eef1c045bafec587dfdb4c8
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-08 20:03:39 +0500

    very basic concepts of Python

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-# print("HELLO WORD!\nhelloworld\nhello world")
-# print("Hello" +" "+ "Jawad")
-# print("Hello" +" "+ "Jawad")
-# print("Hello" +" "+ "Jawad")
-#
-# #---------------------String Manipulation
-#
-# print("Day 1 String Manipulation")
-# print('String Concatenation is done with the "+" sign.')
-# print('e.g("print hello + world")')
-# print("new lines can be created with a back slash and n.")
+print("HELLO WORD!\nhelloworld\nhello world")
+print("Hello" +" "+ "Jawad")
+print("Hello" +" "+ "Jawad")
+print("Hello" +" "+ "Jawad")
 
-# input("what is your name?")
-# print("Hello" + input("what is your name"))
+#---------------------String Manipulation
 
-# input("what is your name")
-# print(len(input("what is your name?")))
-# print(len(input("what is your name? ")))
-# print(len(input("what is your name?")))
-# print(input(len("what is your name? ")))
+print("Day 1 String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g("print hello + world")')
+print("new lines can be created with a back slash and n.")
 
-#------------------------variables----------------------------------------
-# name = input("what is your name? ")
-# len(name)
-# print(len(name))
-#
-# print("hello" + name+ "have a good day")
-# name = "jack"
-# print(name)
-#
-# name = input("what is your name? ")
-# length = len(name)
-# print(length)
-#-----------------------------------------------------------------------------
+input("what is your name?")
+print("Hello" + input("what is your name"))
+
+input("what is your name")
+print(len(input("what is your name?")))
+print(len(input("what is your name? ")))
+print(len(input("what is your name?")))
+print(input(len("what is your name? ")))
+
+# ------------------------variables----------------------------------------
+name = input("what is your name? ")
+len(name)
+print(len(name))
+
+print("hello" + name+ "have a good day")
+name = "jack"
+print(name)
+
+name = input("what is your name? ")
+length = len(name)
+print(length)
+# -----------------------------------------------------------------------------
 
 
 a = input("a:")
@@ -41,4 +41,3 @@ d=a
 print("a:"+c)
 print("b:"+d)
 
-a = input("my name is jawad")
