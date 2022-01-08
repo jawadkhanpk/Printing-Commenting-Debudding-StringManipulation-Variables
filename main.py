31a00b6491f5fb68661b57ba0c9d9d8302b6faea
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-01-08 19:41:56 +0500

    very basic concepts of Python

---
+++ main.py
@@ -0,0 +1,42 @@
+# print("HELLO WORD!\nhelloworld\nhello world")
+# print("Hello" +" "+ "Jawad")
+# print("Hello" +" "+ "Jawad")
+# print("Hello" +" "+ "Jawad")
+#
+# #---------------------String Manipulation
+#
+# print("Day 1 String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g("print hello + world")')
+# print("new lines can be created with a back slash and n.")
+
+# input("what is your name?")
+# print("Hello" + input("what is your name"))
+
+# input("what is your name")
+# print(len(input("what is your name?")))
+# print(len(input("what is your name? ")))
+# print(len(input("what is your name?")))
+# print(input(len("what is your name? ")))
+
+#------------------------variables----------------------------------------
+# name = input("what is your name? ")
+# len(name)
+# print(len(name))
+#
+# print("hello" + name+ "have a good day")
+# name = "jack"
+# print(name)
+#
+# name = input("what is your name? ")
+# length = len(name)
+# print(length)
+#-----------------------------------------------------------------------------
+
+
+a = input("a:")
+b = input("b:")
+c=b
+d=a
+print("a:"+c)
+print("b:"+d)
